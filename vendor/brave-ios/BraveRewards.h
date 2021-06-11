eff1610809cd0220f2f59d02cbe64dd9853c9c3d
Kyle Hickinson <git@kylehickinson.com>
2021-06-11 12:23:22 -0400

    iOS wallet wrappers

--- vendor/brave-ios/BraveRewards.h
+++ vendor/brave-ios/BraveRewards.h
@@ -33,3 +33,8 @@ FOUNDATION_EXPORT const unsigned char BraveRewardsVersionString[];
 #import <BraveRewards/brave_bookmarks_observer.h>
 #import <BraveRewards/brave_bookmarks_importer.h>
 #import <BraveRewards/brave_bookmarks_exporter.h>
+
+// Wallet
+#import <BraveRewards/eth_json_rpc_controller.h>
+#import <BraveRewards/hd_keyring.h>
+#import <BraveRewards/keyring_controller.h>
